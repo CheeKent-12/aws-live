6821ac50da08512d9d156acb5269074947273d82
CheeKent-12 <109887613+CheeKent-12@users.noreply.github.com>
2024-08-30 22:27:41 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cee2y49zkcp2.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limcheekent-employee"
+customregion = "us-east-1"
+
